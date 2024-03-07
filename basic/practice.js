969f5d1786e1bf062febe0475c87bfeae7cb73f5
rohitrodge <webrohit27@gmail.com>
2024-03-07 19:31:23 +0530

    practice question

---
+++ basic/practice.js
@@ -0,0 +1,10 @@
+let x = 3 > 2 > 1 ;
+// console.log(x); // False
+
+let a = 1;
+a = a + (a += 2);
+//console.log(a); // 4
+
+const arr = [1, 2, 3];
+const result = arr[8];
+console.log(result); // undefined
