8b65b2b0f09b972ee2511b366c95602b716feebb
rohitrodge <webrohit27@gmail.com>
2024-03-07 22:22:31 +0530

    push arr

--- basic/linkList.js
+++ basic/linkList.js
@@ -22,10 +22,10 @@ class singlyLinkList{
     this.tail = newNode;
     this.length = 1;
    }
-}
 
-let first = new singlyLinkList(10);
-console.log(first); 
+
+//let first = new singlyLinkList(10);
+//console.log(first); 
 
 // Output
 /* singlyLinkList {
@@ -33,3 +33,32 @@ console.log(first);
   tail: Node { val: 10, next: null },
   length: 1
 } */
+
+// [10, 20, 30, 40] now here we push 50.
+
+push(val) {
+   const newNode = new Node(val);
+   if(!this.head){
+    this.head = newNode;
+    this.tail = this.head;
+   }
+   
+   else{
+    this.tail.next = newNode;
+   }
+
+   this.length++;
+   return this;
+}
+}
+
+let arr = new singlyLinkList(25);
+console.log(arr.push(100));
+console.log(arr.push(150));
+
+// Output
+/* singlyLinkList {
+    head: Node { val: 25, next: Node { val: 150, next: null } },
+    tail: Node { val: 25, next: Node { val: 150, next: null } },
+    length: 3
+  } */
